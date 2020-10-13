f2a41fe6f387fbf11c643f711e1fda7883dad8e4
root <root@video.yellco.ru>
2020-10-13 23:05:06 +0300

    Fix cfg tokens

--- tasksbot.py
+++ tasksbot.py
@@ -3,12 +3,12 @@ import telebot
 
 cfg = config.Config('config.cfg')
 
-bot = telebot.TeleBot(cfg['token'])
+bot = telebot.TeleBot(cfg['telegram_token'])
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def repeat_all_messages(message):
+	bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-    print('Запущен')
-    bot.polling(none_stop=True)
+	print('Запущен')
+	bot.polling(none_stop=True)
