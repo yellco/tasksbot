f23a2f16c08421f216a421053bda9c0e61bf8b04
yellco <dazheltyy@gmail.com>
2020-10-16 02:15:41 +0300

    Fix creating board and list

--- tasksbot.py
+++ tasksbot.py
@@ -13,47 +13,47 @@ from datetime import datetime
 query = {
    'key': cfg['trello_key'],
    'token': '',
-   'idList': cfg['idList'],
-   'idBoard': cfg['idBoard']
+   'idList': '',
+   'idBoard': ''
 }
 
 token = ''
+idBoard = ''
+idList = ''
+
+def create_list():
+	global token, idBoard
+	querystring = {"name": "Tasks", "key": cfg['trello_key'], "token": token}
+	response = requests.post("https://api.trello.com/1/boards/" + idBoard + "/lists", params=querystring)
+	print(response.text)
+	return response.json()['id']
 
 def create_board():
 	global token
-	url = "https://api.trello.com/1/boards/"
-
-	query = {
-		'key': cfg['trello_key'],
-		'token': token,
-		'name': 'Tasksbot доска'
-	}
-
-	response = requests.request(
-		"POST",
-		url,
-		params=query
-	)
-
+	querystring = {"name": "Telegram tasksbot", "key": cfg['trello_key'], "token": token, 'defaultsLists': False}
+	response = requests.post("https://api.trello.com/1/boards/", params=querystring)
 	print(response.text)
+	return response.json()['id']
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
 	bot.send_message(message.chat.id, "Привет. Для начала давай настроим приложение. ")
 	keyboard = telebot.types.InlineKeyboardMarkup()
-	url_button = telebot.types.InlineKeyboardButton(text="Авторизация trello", url="https://trello.com/1/authorize?expiration=1day&name=Telegram tasksbot&scope=read,write,account&response_type=token&key=" + cfg['trello_key'])
+	url_button = telebot.types.InlineKeyboardButton(text="Авторизация trello", url="https://trello.com/1/authorize?expiration=30days&name=Telegram+tasksbot&scope=read,write&response_type=token&key=" + cfg['trello_key'])
 	keyboard.add(url_button)
 	bot.send_message(message.chat.id, "Сначала авторизуй приложение по ссылке и пришли мне токен.", reply_markup=keyboard)
 
 @bot.message_handler(content_types=["text"])
 def check_messages(message):
-	global token
+	global token, idBoard, idList
 	if len(query['token']) > 0:
 		full_message = message.text.split('..')
 		print(full_message)
 		if len(full_message) > 1:
 			query['name'] = full_message[0]
 			query['desc'] = full_message[1]
+			query['idList'] = idList
+			query['idBoard'] = idBoard
 
 			response = requests.request(
 				"POST",
@@ -70,9 +70,11 @@ def check_messages(message):
 	elif len(query['token']) == 0:
 		if len(message.text) == 64:
 			bot.send_message(message.chat.id, 'Token принят. Давай создадим новую доску.')
-			# create_board()
-			query['token'] = message.text
 			token = message.text
+			idBoard = create_board()
+			idList = create_list()
+			query['token'] = message.text
+			bot.send_message(message.chat.id, 'Доска создана и список задач тоже. Ты их можешь найти у себя в трелло под названием Telegram tasksbot и Tasks соответственно. Теперь присылай информацию для создания карточки вида "Название карточки..Описание карточки"')
 			# bot.send_message(message.chat.id, 'Теперь пришли мне id доски куда хочешь слать карточки.')
 		else:
 			keyboard = telebot.types.InlineKeyboardMarkup()
